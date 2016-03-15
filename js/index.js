97cd7866d3fa3389d5b9dad8a74f62c5cb5dc32a
octocrux <cate.petrusevich@gmail.com>
2016-03-15 17:47:13 +0300

    Modify controls to support algorithm selection

--- js/index.js
+++ js/index.js
@@ -1,48 +1,90 @@
 (function (root) {
     var continueSimulation = false;
+    var started = false;
     var timer;
-    var solutionState = {
-        maze: root.maze.MAZE_Y,
-        currentPoint: [1, 0],
-        path: [],
-        pathFound: false,
-        currentDirection: root.maze.DOWN
-    };
+    var interval = 30;
+    var solution;
+    var solutionState;
+
+    function resetState () {
+        var state = {
+            currentPoint: [1, 0],
+            currentDirection: root.maze.DOWN,
+            currentWeight: 0,
+            startingPoint: [1, 0],
+            checkList: [[1, 0]],
+            path: [],
+            pathFound: false,
+            maze: copyMaze(selectMaze())
+        };
+        return state;
+    }
+
+    function copyMaze (maze) {
+        var newMaze = [];
+        for (var i = 0; i < maze.length; i++) {
+            newMaze[i] = [];
+            for (var j = 0; j < maze[0].length; j++) {
+                newMaze[i].push(maze[i][j]);
+            }
+        }
+        return newMaze;
+    }
 
     function selectMaze () {
-        var selectedMaze = document.querySelector('.controls__selector').value;
-
+        var selectedMaze = document.getElementById('pattern').value;
+        var maze;
         switch (selectedMaze) {
-        case 'mazeY':
-            solutionState.maze = root.maze.MAZE_Y;
-            break;
-        case 'maze21':
-            solutionState.maze = root.maze.MAZE_21;
-            break;
-        case 'maze31':
-            solutionState.maze = root.maze.MAZE_31;
-            break;
-        case 'maze51':
-            solutionState.maze = root.maze.MAZE_51;
-            break;
-        default:
-            solutionState.maze = root.maze.MAZE_Y;
-            break;
+            case 'maze21':
+                maze = root.maze.MAZE_21;
+                break;
+
+            case 'maze31':
+                maze = root.maze.MAZE_31;
+                break;
+
+            case 'maze51':
+                maze = root.maze.MAZE_51;
+                break;
+
+            default:
+                maze = root.maze.MAZE_Y;
+                break;
         }
+        return maze;
     }
+
+    function selectAlgorithm () {
+        var selectedAlgorithm = document.getElementById('algorithm').value;
+        var algorithm;
+        switch (selectedAlgorithm) {
+            case 'dijkstra':
+                algorithm = root.maze.solutionDijkstra;
+                break;
+
+            default:
+                algorithm = root.maze.solutionRightHand;
+                break;
+        }
+        return algorithm;
+    }
+
+    var algorithmSelector = document.getElementById('algorithm');
+    algorithmSelector.onchange = function () {
+        solution = selectAlgorithm();
+    };
 
     var mazeSelector = document.getElementById('pattern');
     mazeSelector.onchange = function () {
-        debugger;
-        selectMaze();
+        solutionState.maze = selectMaze();
         root.maze.redraw(solutionState.maze);
-    }
+    };
 
     var runButton = document.getElementById('run');
     runButton.onclick = function() {
         continueSimulation = !continueSimulation;
         if (continueSimulation){
-            timer = setInterval(root.maze.solution, 50, solutionState);
+            timer = setInterval(solution, interval, solutionState);
             runButton.textContent = 'Пауза';
         }
         else {
@@ -52,36 +94,34 @@
     };
 
     var restartButton = document.getElementById('restart');
-    var started = false;
     restartButton.onclick = function () {
         started = !started;
+        clearInterval(timer);
 
         if (started) {
-            clearInterval(timer);
             continueSimulation = true;
+            solutionState = resetState();
 
-            solutionState.currentPoint = [1, 0];
-            solutionState.path = [];
-            solutionState.pathFound = false;
-            solutionState.currentDirection = root.maze.DOWN;
-
-            selectMaze();
             mazeSelector.disabled = true;
-            timer = setInterval(root.maze.solution, 50, solutionState);
+            algorithmSelector.disabled = true;
             runButton.style.display = 'inline';
             runButton.textContent = 'Пауза';
-
             restartButton.textContent = 'Стоп';
+
+            timer = setInterval(solution, interval, solutionState);
         } else {
-            clearInterval(timer);
             continueSimulation = false;
+
             mazeSelector.disabled = false;
+            algorithmSelector.disabled = false;
             runButton.style.display = 'none';
             restartButton.textContent = 'Старт';
-            root.maze.redraw(solutionState.maze);
+            root.maze.redraw(selectMaze());
         }
     };
 
     runButton.style.display = 'none';
+    solutionState = resetState();
+    solution = selectAlgorithm();
     root.maze.redraw(solutionState.maze);
 })(this);
